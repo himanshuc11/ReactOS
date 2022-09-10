49691695c0bd7e5f82ede009843056df226f5743
himanshuc11 <himanshuchhatpar@gmail.com>
2022-09-10 23:10:16 +0530

    Configured Styled-Components to work in SSR and CSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,22 @@
-function Home() {
-  return <h1 >Hello World</h1>;
+import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html, body {
+  margin: 0;
+  padding: 0;
 }
 
-export default Home;
+* {
+  box-sizing: border-box;
+}
+`;
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}

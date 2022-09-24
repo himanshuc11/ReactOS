53dd4884445e7fb428ae398407cf639288ce6301
himanshuc11 <himanshuchhatpar@gmail.com>
2022-09-24 19:56:05 +0530

    Refactored stylelint

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,27 +6,22 @@ import Document, {
   NextScript
 } from 'next/document';
 import { ReactElement } from 'react';
-import { ServerStyleSheet } from 'styled-components';
+import withStyleSheet from 'utils/withStyleSheet';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    return withStyleSheet(ctx);
+  }
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()]
-      };
-    } finally {
-      sheet.seal();
-    }
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
